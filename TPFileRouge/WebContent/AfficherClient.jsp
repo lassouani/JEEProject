e8abc61cc3e98c7d5fea89c71c0e0aef1766420b
Lassouani <lassaouni_sofiane@outlook.com>
2016-11-30 21:19:52 +0100

    firt commit

---
+++ TPFileRouge/WebContent/AfficherClient.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+ 
+<title>Afficher Client</title>
+<link type="text/css" rel="stylesheet" href="inc/style.css" />
+</head>
+	<body>
+        <%-- Affichage de la chaîne "message" transmise par la servlet --%>
+        <p class="info">${ message }</p>
+        <%-- Puis affichage des données enregistrées dans le bean "client" transmis par la servlet --%>
+        <p>Nom : ${ client.nom }</p>
+        <p>Prénom : ${ client.prenom }</p>
+        <p>Adresse : ${ client.adresse }</p>
+        <p>Numéro de téléphone : ${ client.telephone }</p>
+        <p>Email : ${ client.email }</p>
+        
+         <p class="info">${ test }</p>
+    </body>
+	
+</html>
---
+++ TPFileRouge/src/com/servlets/CreationClient.java
@@ -0,0 +1,86 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.beans.*;
+
+/**
+ * Servlet implementation class CreationClient
+ */
+@WebServlet("/CreationClient")
+public class CreationClient extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreationClient() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		/*
+         * Récupération des données saisies, envoyées en tant que paramètres de
+         * la requête GET générée à la validation du formulaire Client.jsp
+         */
+        String nom = request.getParameter( "nomClient" );
+        String prenom = request.getParameter( "prenomClient" );
+        String adresse = request.getParameter( "adresseClient" );
+        String telephone = request.getParameter( "telephoneClient" );
+        String email = request.getParameter( "emailClient" );
+        
+        
+        String message;
+        /*
+         * Initialisation du message à afficher : si un des champs obligatoires
+         * du formulaire n'est pas renseigné, alors on affiche un message
+         * d'erreur, sinon on affiche un message de succès
+         */
+        if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() ) {
+            message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"Client.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un client.";
+        } else {
+            message = "Client créé avec succès !";
+        }
+        
+        /*
+         * Création du bean Client et initialisation avec les données récupérées
+         */
+        Client client = new Client();
+        client.setNom( nom );
+        client.setPrenom( prenom );
+        client.setAdresse( adresse );
+        client.setTelephone( telephone );
+        client.setEmail( email );
+       
+        
+        /* essayer de passer un lien de creation de commande */
+        String test= "<a href=\"Commande.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'une commande";
+        
+        /* Ajout du bean et du message à l'objet requête */
+        request.setAttribute( "client", client );
+        request.setAttribute( "message", message );
+        request.setAttribute( "test", test );
+        
+        
+        /* Transmission à la page JSP en charge de l'affichage des données */
+        this.getServletContext().getRequestDispatcher( "/AfficherClient.jsp" ).forward( request, response );
+
+        
+
+	}
+
+	
+
+}
